77395a2c1c1a215408b11a78134bc5ecea7a60ca
Artem Lipchik <tommerg09@gmail.com>
2019-03-15 11:25:38 +0300

    I write a method makeExchange that determine the minimum number of coins needed to make change for a given amount in American currency

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
-}
+    var count = {};
+    if (currency <= 0) {
+      return count;
+    }
+    else if (currency > 10000) {
+      count.error = "You are rich, my friend! We don't have so much coins for exchange";
+      return count; 
+    }
+    else {
+      
+        if (currency >= 50) {
+          count.H = Math.floor(currency/50);
+          currency = currency%50;
+        }  
+
+        if (currency >= 25) {
+          count.Q = Math.floor(currency/25);
+          currency = currency%25;
+        }  
+
+        if (currency >= 10) {
+          count.D = Math.floor(currency/10);
+          currency = currency%10;
+        }  
+
+        if (currency >= 5) {
+
+          count.N = Math.floor(currency/5);
+          currency = currency%5;
+
+        }  
+
+        if (currency > 0) {
+        count.P = Math.floor(currency/1);
+        }
+
+    }
+
+    return count;
+
+  }
